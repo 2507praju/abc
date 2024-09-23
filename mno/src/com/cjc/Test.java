0972ef2aa36185885d882aba4ad67a2888587fce
VS <VS@DESKTOP-PBN0VBD>
2024-09-23 18:40:54 +0530

    by PRAJU

---
+++ mno/src/com/cjc/Test.java
@@ -0,0 +1,14 @@
+package com.cjc;
+
+public class Test {
+	
+	public void m1() {
+		System.out.println("this m1");
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("this is main method");
+	}
+
+}
